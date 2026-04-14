ea2199a832164204c9cf5a984b6cfe231997f3f7
sumancx31 <sumansah029@gmail.com>
2026-04-14 20:46:51 +0545

    register problem solved

--- src/modules/chat/chat.router.js
+++
@@ -1,9 +0,0 @@
-const ChatRouter = require("express").Router();
-const chatCtrl = require("./chat.controller");
-const auth = require("../../middlewares/auth.middleware");
-
-// Attach auth middleware correctly (without calling it)
-ChatRouter.get("/:receiverId", auth, chatCtrl.getChatHistory);
-ChatRouter.post("/message", auth, chatCtrl.sendMessage);
-
-module.exports = ChatRouter;
