324189cc24dd60471e702f0425dc3aab7b85e44f
sumancx31 <sumansah029@gmail.com>
2026-04-14 20:54:42 +0545

    register problem solved

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -2,7 +2,6 @@ const driverRoute = require("../modules/admin/driver.route");
 const authRouter = require("../modules/auth/auth.router");
 const bannerRouter = require("../modules/banner/banner.router");
 const busRouter = require("../modules/bus/bus.router");
-const ChatRouter = require("../modules/chat/chat.router");
 const OfferRouter = require("../modules/offers/offers.router");
 const OrderRouter = require("../modules/order/order.router");
 const bookingRouter = require("../modules/reservation/booking.route");
@@ -27,7 +26,6 @@ router.use("/trip-update",TripUpdateRouter);
 router.use("/book-ticket",bookingRouter);
 router.use("/driver-bus",driverRoute);
 router.use("/order",OrderRouter);
-router.use("/chat",ChatRouter);
 router.use("/offers",OfferRouter);
 
 module.exports = router;
